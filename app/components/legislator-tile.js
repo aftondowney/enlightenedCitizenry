2b34da0290a14126131d94779ef652fba2050ae4
Afton Downey <aaike001@gmail.com>
2016-04-05 09:05:21 -0700

    add api call and template

--- app/components/legislator-tile.js
+++ app/components/legislator-tile.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  model: function(params) {
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?zip=' + params.zip + '&apikey=6ba17065d6c0455689bc684933e2792e';
+    return Ember.$.getJSON(url).then(function(responseJSON) {
+      return responseJSON.results;
+    });
+  }
 });
